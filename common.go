d82a5646e3ccda130cd23b6c9ff82bde797300c8
Sam Delmerico <svdelmerico@gmail.com>
2014-01-03 16:43:05 -0500

    added templating for the home page in order to render posts
    The html/css looks pretty bad, so that needs to be cleaned up and made
    to look pretty. However, now there's a system in place to render the
    posts that are saved to the datastore.

--- common.go
+++ common.go
@@ -8,3 +8,13 @@ type Post struct {
 	Author, Title, Text string
 	Date                time.Time
 }
+
+type Posts []*Post
+
+func (p Posts) Less(i, j int) bool {
+	return int(p[i].Date.Sub(p[j].Date)) < 0
+}
+
+func (p Posts) Len() int { return len(p) }
+
+func (p Posts) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
--- render.go
+++ render.go
@@ -2,43 +2,86 @@ package goblog
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
-	"time"
+	"sort"
+	//"time"
 
 	"appengine"
 	"appengine/datastore"
 )
 
+const renderTemplateHTML = `
+<div class="post">
+	<div class="post_header">written by <b>{{.Author}}</b> on <b>{{.Date}}</b></div>
+	<br/>
+	<div class="post_text">{{.Text}}</div>
+</div>
+`
+
+const siteHeader = `
+<html>
+	<head>
+		<title>Goblog Testing Site</title>	
+		<link rel="stylesheet" type="text/css" href="/stylesheets/main.css"/>
+	</head>
+	<body>
+		<div id="top_bar">
+			<div id="header"><a href="/">Goblog Testing Site</a></div>
+			<div id="header_accent"></div>
+		</div>
+	<div id="main">
+		<div id="sidebar">
+			This is a testing site for the sick Goblog blogging.
+		</div>
+`
+const siteFooter = `
+	</div>
+	</body>
+</html>
+`
+
+var renderTemplate = template.Must(template.New("post").Parse(renderTemplateHTML))
+
 func RenderAllPosts(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 
-	p1 := Post{
-		"Joe",
-		"Title1",
-		"This is an awesome blog.",
-		time.Now(),
-	}
-	fmt.Fprintln(w, p1)
+	fmt.Fprintf(w, siteHeader)
 
-	_, err := datastore.Put(c, datastore.NewIncompleteKey(c, "post", nil), &p1)
-	if err != nil {
-		fmt.Fprintf(w, "ERROR. datastore.Put() returned `%v`\n", err)
-	}
+	//p1 := Post{
+	//"Joe Smith",
+	//"Title1",
+	//"This is an awesome blog.",
+	//time.Now(),
+	//}
+	//fmt.Fprintf(w, "<p>%v</p>", p1)
+
+	//_, err := datastore.Put(c, datastore.NewIncompleteKey(c, "post", nil), &p1)
+	//if err != nil {
+	//fmt.Fprintf(w, "<p>ERROR. datastore.Put() returned `%v`</p>", err)
+	//}
 
 	q := datastore.NewQuery("post")
-	count, err := q.Count(c)
-	if err != nil {
-		fmt.Fprintf(w, "ERROR. q.Count() returned '%v'\n", err)
-	}
-	fmt.Fprintln(w, count)
+	//count, err := q.Count(c)
+	//if err != nil {
+	//fmt.Fprintf(w, "<p>ERROR. q.Count() returned '%v'</p>", err)
+	//}
 
-	var allPosts []*Post
-	_, err = q.GetAll(c, &allPosts)
+	var allPosts Posts
+	_, err := q.GetAll(c, &allPosts)
 	if err != nil {
-		fmt.Fprintf(w, "ERROR. q.GetAll() returned `%v`\n", err)
+		fmt.Fprintf(w, "<p>ERROR. q.GetAll() returned `%v`</p>", err)
 	}
+
+	sort.Sort(sort.Reverse(allPosts))
 
 	for i := range allPosts {
-		fmt.Fprintln(w, *allPosts[i])
+		//fmt.Fprintln(w, *allPosts[i])
+		if err := renderTemplate.Execute(w, allPosts[i]); err != nil {
+			fmt.Fprintf(w, "<p>ERROR. renderTemplate.Execute() on post #%v returned `%v`</p>", i, err)
+		}
 	}
+
+	//fmt.Fprintf(w, "<p>%v</p>", count)
+	fmt.Fprintf(w, siteFooter)
 }
