443a32d9525e860460cd29b0af144f1bf351eb37
Sam Delmerico <svdelmerico@gmail.com>
2014-01-05 22:42:25 -0500

    changed the rendering function to allow rendering of some, all, or a single post

--- render.go
+++ render.go
@@ -6,31 +6,59 @@ import (
 	htemplate "html/template"
 	"net/http"
 	"sort"
+	"strings"
 	ttemplate "text/template"
 
 	"appengine"
 	"appengine/datastore"
 )
 
+func init() {
+	http.HandleFunc("/", renderPosts)
+}
+
 var siteHeaderTemplate = ttemplate.Must(ttemplate.New("").ParseFiles("templates/header.html"))
 var homePageBodyTemplate = htemplate.Must(htemplate.New("").ParseFiles("templates/homepage.html"))
 
-func RenderAllPosts(w http.ResponseWriter, r *http.Request) {
+func renderPosts(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
+
+	splitURL := strings.SplitAfter(r.URL.String(), "/")
+	// the split slice ends with a blank entry
+	postNum := splitURL[len(splitURL)-2]
+	if postNum != "/" {
+		fmt.Fprintf(w, "other url: %v\n", splitURL)
+		return
+	}
+
+	fmt.Fprintf(w, renderPost(c, -1, 3))
+}
+
+// renderPost renders a single post if postID != -1, or it renders the specified number of posts (newest first) if numToRender != -1.
+// If both parameters are -1, then it renders all posts (newest first).
+// The function returns a string of the resulting templated HTML.
+func renderPost(c appengine.Context, postID, numToRender int) string {
 	hpb := datatypes.WebPageBody{}
+	finalHpb := datatypes.WebPageBody{}
 
 	q := datastore.NewQuery("post")
 	var allPosts []*datatypes.Post
 	_, err := q.GetAll(c, &allPosts)
 	if err != nil {
-		fmt.Fprintf(w, "<p>ERROR. q.GetAll() returned `%v`</p>", err)
+		return fmt.Sprintf("<p>ERROR. q.GetAll() returned `%v`</p>", err)
+	}
+	sort.Sort(sort.Reverse(datatypes.Posts(allPosts)))
+
+	if len(allPosts) > numToRender && numToRender != -1 {
+		allPosts = allPosts[:numToRender]
 	}
 
-	sort.Sort(sort.Reverse(datatypes.Posts(allPosts)))
 	if err := homePageBodyTemplate.ExecuteTemplate(&hpb, "homepage.html", allPosts); err != nil {
-		fmt.Fprintf(w, "<p>ERROR. renderTemplate.Execute() returned `%v`</p>", err)
+		return fmt.Sprintf("<p>ERROR. renderTemplate.Execute() returned `%v`</p>", err)
 	}
-	if err := siteHeaderTemplate.ExecuteTemplate(w, "header.html", hpb); err != nil {
-		fmt.Fprintf(w, "<p>ERROR. renderTemplate.Execute() returned `%v`</p>", err)
+	if err := siteHeaderTemplate.ExecuteTemplate(&finalHpb, "header.html", hpb); err != nil {
+		return fmt.Sprintf("<p>ERROR. renderTemplate.Execute() returned `%v`</p>", err)
 	}
+
+	return finalHpb.HTMLBody
 }
